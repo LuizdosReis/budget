1189daa72c33f7eadea6c472daaa3eb85baa04b8
Luiz dos Reis <luizdosreis@gmail.com>
2022-06-09 17:42:54 -0300

    fix components folder

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
-import { PageComponent } from './page/page.component';
-import { FooterComponent } from './footer/footer.component';
+import { PageComponent } from './components/page/page.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent, PageComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, PageComponent],
   imports: [CommonModule, RouterModule],
   exports: [CommonModule, RouterModule, PageComponent],
 })
