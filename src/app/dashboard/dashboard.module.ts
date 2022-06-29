edaf17e9fb06ba8603126a3af86c0356b1b9e323
Luiz dos Reis <luizdosreis@gmail.com>
2022-06-29 13:48:51 -0300

    create account card component

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,9 +5,14 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SwiperModule } from 'swiper/angular';
 import { MonthSwiperComponent } from './components/month-swiper/month-swiper.component';
+import { AccountCardComponent } from './components/account-card/account-card.component';
 
 @NgModule({
-  declarations: [DashboardComponent, MonthSwiperComponent],
+  declarations: [
+    DashboardComponent,
+    MonthSwiperComponent,
+    AccountCardComponent,
+  ],
   imports: [DashboardRoutingModule, SharedModule, SwiperModule],
 })
 export class DashboardModule {}
