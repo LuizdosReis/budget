36865b8b2bf2733a4b001e571db03c74cf2deb89
Luiz dos Reis <luizdosreis@gmail.com>
2022-07-13 18:39:42 -0300

    testing the return of dashboardServiceApi on dashboard component

--- src/app/dashboard/pages/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/pages/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
 import Account from '../../models/accounts';
 import { DashboardApiService } from '../../services/dashboard-api.service';
@@ -19,20 +20,26 @@ describe('DashboardComponent', () => {
       deposits: [],
       withdrawals: [],
     }),
+    new Account({
+      name: 'Nubank',
+      currencyCode: 'BRL',
+      currentBalance: 5,
+      expectedBalance: 20,
+      monthlyBalance: 2,
+      deposits: [],
+      withdrawals: [],
+    }),
   ];
 
-  const fakeDashboardApiService = jasmine.createSpyObj<DashboardApiService>(
-    'dashboardApiService',
-    {
-      getAccounts: of(accounts),
-    }
-  );
+  const dashboardApiServiceSpy = jasmine.createSpyObj('DashboardApiService', [
+    'getAccounts',
+  ]);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DashboardComponent],
       providers: [
-        { provide: DashboardApiService, useValue: fakeDashboardApiService },
+        { provide: DashboardApiService, useValue: dashboardApiServiceSpy },
       ],
     }).compileComponents();
   });
@@ -40,10 +47,30 @@ describe('DashboardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('accounts in component should be equal the accounts return by dashboardApiService', () => {
+    dashboardApiServiceSpy.getAccounts.and.returnValue(of(accounts));
+    fixture.detectChanges();
+
+    expect(component.accounts).toBe(accounts);
+  });
+
+  it('accounts and accounts card should have the same length', () => {
+    dashboardApiServiceSpy.getAccounts.and.returnValue(of(accounts));
+    fixture.detectChanges();
+
+    expect(
+      fixture.debugElement.queryAll(By.css('app-account-card')).length
+    ).toBe(accounts.length);
+  });
+
+  it('should not have accounts cards when dashboardApiService getAccounts does not return accounts', () => {
+    dashboardApiServiceSpy.getAccounts.and.returnValue(of([]));
+    fixture.detectChanges();
+
+    expect(
+      fixture.debugElement.queryAll(By.css('app-account-card')).length
+    ).toBe(0);
   });
 });
