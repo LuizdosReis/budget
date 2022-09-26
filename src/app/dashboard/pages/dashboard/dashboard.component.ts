f8dddf484e929552e85a50957d4e48dad5bb8823
Luiz dos Reis <luizdosreis@gmail.com>
2022-09-26 20:05:04 -0300

    create unsub component

--- src/app/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { takeUntil } from 'rxjs';
+import { UnsubComponent } from 'src/app/shared/components/unsub.component';
 import { Account } from '../../models/accounts';
 import { MonthYear } from '../../models/monthYear';
 import { DashboardApiService } from '../../services/dashboard-api.service';
@@ -8,14 +10,16 @@ import { DashboardApiService } from '../../services/dashboard-api.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent extends UnsubComponent implements OnInit {
   accounts?: Account[];
   accountsLoaded = false;
   monthsYearsLoaded = false;
   monthsYears: MonthYear[] = [];
   currentMonthYear!: MonthYear;
 
-  constructor(private dashboardApi: DashboardApiService) {}
+  constructor(private dashboardApi: DashboardApiService) {
+    super();
+  }
 
   ngOnInit(): void {
     this.loadMonthsYears();
@@ -30,18 +34,24 @@ export class DashboardComponent implements OnInit {
   }
 
   loadMonthsYears(): void {
-    this.dashboardApi.getMonthsYears().subscribe((monthsYears: MonthYear[]) => {
-      this.monthsYears = monthsYears;
-      this.monthsYearsLoaded = true;
-    });
+    this.dashboardApi
+      .getMonthsYears()
+      .pipe(takeUntil(this.notifier))
+      .subscribe((monthsYears: MonthYear[]) => {
+        this.monthsYears = monthsYears;
+        this.monthsYearsLoaded = true;
+      });
   }
 
   loadAccounts(): void {
     this.accountsLoaded = false;
-    this.dashboardApi.getAccounts(this.currentMonthYear).subscribe(accounts => {
-      this.accounts = accounts;
-      this.accountsLoaded = true;
-    });
+    this.dashboardApi
+      .getAccounts(this.currentMonthYear)
+      .pipe(takeUntil(this.notifier))
+      .subscribe(accounts => {
+        this.accounts = accounts;
+        this.accountsLoaded = true;
+      });
   }
 
   selectMonthYear(monthYear: MonthYear): void {
