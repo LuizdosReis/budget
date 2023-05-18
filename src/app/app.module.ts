4fcfc340a926af602e130d9fe71691af8934b58f
Luiz dos Reis <luizdosreis@gmail.com>
2023-05-18 22:08:10 +0100

    remove unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
