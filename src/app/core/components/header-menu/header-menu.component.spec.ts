825b6b318f204843691d292c304c9bc0a2a5df9c
Luiz dos Reis <luizdosreis@gmail.com>
2024-08-19 23:19:46 +0100

    refactory header menu tests and add click out test

--- src/app/core/components/header-menu/header-menu.component.spec.ts
+++ src/app/core/components/header-menu/header-menu.component.spec.ts
@@ -1,43 +1,50 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-
+import { byTestId, createHostFactory, Spectator } from '@ngneat/spectator';
 import { HeaderMenuComponent } from './header-menu.component';
 
 describe('HeaderMenuComponent', () => {
-  let component: HeaderMenuComponent;
-  let fixture: ComponentFixture<HeaderMenuComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [HeaderMenuComponent],
-    }).compileComponents();
+  let spectator: Spectator<HeaderMenuComponent>;
+  const createHost = createHostFactory<HeaderMenuComponent>({
+    component: HeaderMenuComponent,
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderMenuComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createHost(
+      '<div data-testid="header-menu-wrapper"><app-header-menu></app-header-menu></div>'
+    );
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
   });
 
-  it('should change isMenuOpen to true when click on the button twice', () => {
-    fixture.debugElement
-      .query(By.css('button'))
-      .triggerEventHandler('click', null);
-
-    expect(component.isMenuOpen).toBeTrue();
+  it('should start with the class -left-1/2', () => {
+    expect(spectator.query(byTestId('app-menu'))).not.toHaveClass('left-0');
+    expect(spectator.query(byTestId('app-menu'))).toHaveClass('-left-1/2');
   });
 
-  it('should change isMenuOpen to false when click on the button twice', () => {
+  it('should have the class left-0 when click on the button', () => {
+    spectator.click('button');
+
+    expect(spectator.query(byTestId('app-menu'))).toHaveClass('left-0');
+    expect(spectator.query(byTestId('app-menu'))).not.toHaveClass('-left-1/2');
+  });
+
+  it('should have the class -left-1/2 when click on the button twice', () => {
     for (let i = 1; i <= 2; i++) {
-      fixture.debugElement
-        .query(By.css('button'))
-        .triggerEventHandler('click', null);
+      spectator.click('button');
     }
 
-    expect(component.isMenuOpen).toBeFalse();
+    expect(spectator.query(byTestId('app-menu'))).not.toHaveClass('left-0');
+    expect(spectator.query(byTestId('app-menu'))).toHaveClass('-left-1/2');
+  });
+
+  it('should have the class -left-1/2 when click out', () => {
+    spectator.click('button');
+
+    expect(spectator.query(byTestId('app-menu'))).toHaveClass('left-0');
+
+    spectator.click(byTestId('header-menu-wrapper'));
+
+    expect(spectator.query(byTestId('app-menu'))).toHaveClass('-left-1/2');
   });
 });
