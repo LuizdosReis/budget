7e009314fdb617dd8796edeb463a0fb912a70982
Luiz dos Reis <luizdosreis@gmail.com>
2025-04-04 23:01:17 +0100

    chore: remove unnused menu.components.scss

--- src/app/core/components/menu/menu.component.ts
+++ src/app/core/components/menu/menu.component.ts
@@ -3,7 +3,6 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent {
   @Output() clicked = new EventEmitter<void>();
